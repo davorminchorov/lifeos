16925ab77504a69ee38e35a3cf5a2a5ba323a6cb
Davor Minchorov <davorminchorov@gmail.com>
2024-09-22 20:01:49 +0200

    Install Rector

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,12 +1,16 @@
 <?php
 
-$finder = (new PhpCsFixer\Finder())
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
+
+$finder = (new Finder())
     ->in(__DIR__)
     ->exclude('var')
 ;
 
-return (new PhpCsFixer\Config())
-    ->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect())
+return (new Config())
+    ->setParallelConfig(ParallelConfigFactory::detect())
     ->setRules([
         '@Symfony' => true,
     ])
