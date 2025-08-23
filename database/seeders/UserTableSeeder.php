1c1835ceaaaa017e89711e1fb0144d1667f1ae6b
Davor Minchorov <davorminchorov@gmail.com>
2025-08-23 20:56:45 +0200

    Implement a custom database seeder for the first User

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::firstOrCreate(
+            ['email' => 'davor@davorminchorov.com'],
+            [
+                'name' => 'Davor Minchorov',
+                'email_verified_at' => now(),
+                'password' => bcrypt('password'),
+            ]
+        );
+    }
+}
